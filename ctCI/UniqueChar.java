851bd8216394975f7ce492a6fbe68a5c64d5f979
Rezoana <rezoana.dilshad@gmail.com>
2019-09-17 14:14:04 -0400

    Check if the string has all unique chars (2 methods)

--- ctCI/UniqueChar.java
+++ ctCI/UniqueChar.java
@@ -1,8 +1,16 @@
 package ctCI;
+/**
+ * 1.1 Is Unique
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ */
 
 import java.util.HashMap;
 
 public class UniqueChar {
+	
+	//Using additional data structure Hashmap
+	//O(n) where n is the number of elements
     public static boolean UniqChar(String s) 
     {
         HashMap<Character, Integer> hmap = new HashMap<>();
@@ -20,16 +28,36 @@ public class UniqueChar {
         return true;
     }
     
+    // Without additional data structure
+    //O(n^2)
+    public static boolean isUniqueInPlace(String s) 
+    {
+    	for(int i = 0; i < s.length(); i++)
+    	{
+    		//check if we already saw this element before
+    		for(int j = 0; j < i; j++)
+    		{
+    			if(s.charAt(i) == s.charAt(j))
+    				return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    
     public static void main(String[] args)
     {
+    	//Using the first method
     	String str = "hel";
-    	System.out.println(str + " has unique characters: " +  UniqChar(str) );
+    	System.out.println(str + " has unique characters: " +  UniqChar(str));
     	String str2 = "hello";
-    	System.out.println(str2 + " has unique characters: " +  UniqChar(str2) );
+    	System.out.println(str2 + " has unique characters: " +  UniqChar(str2));
+    	
+    	//Using the 2nd method
     	String str1 = "tick";
-    	System.out.println(str1 + " has unique characters: " +  UniqChar(str1) );
-    	
-    	
+    	System.out.println(str1 + " has unique characters: " +  isUniqueInPlace(str1));
+    	String str3 = "tickt";
+    	System.out.println(str3 + " has unique characters: " +  isUniqueInPlace(str3));
     }
 
 }
